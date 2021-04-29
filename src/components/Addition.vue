37bc821470e45797a1994d83c3cad7a15295a3df
Yincheng <974578444@qq.com>
2021-04-29 23:17:13 +0800

    第二次提交

--- src/components/Addition.vue
+++ src/components/Addition.vue
@@ -1,14 +1,32 @@
 <template>
   <div>
-    <h3>当前最新的count:</h3>
-    <button>+1</button>
+    <!-- <h3>当前最新的count:{{$store.state.count}}</h3> -->
+    <h3>{{$store.getters.showNum}}</h3>
+    <button @click="btnHandler1">+1</button>
+    <button @click="btnHandler2">+N</button>
+    <button @click="btnHandler3">+1 Async</button>
+    <button @click="btnHandler4">+N Async</button>
   </div>
-</template> 
+</template>
 
 <script>
 export default {
-  data() {
+  data () {
     return {}
+  },
+  methods: {
+    btnHandler1 () {
+      this.$store.commit('add')
+    },
+    btnHandler2 () {
+      this.$store.commit('addN', 3)
+    },
+    btnHandler3 () {
+      this.$store.dispatch('addAsync')
+    },
+    btnHandler4() {
+      this.$store.dispatch('addNAsync', 5)
+    }
   }
 }
-</script> 
+</script>
--- src/components/Subtraction.vue
+++ src/components/Subtraction.vue
@@ -1,14 +1,39 @@
 <template>
   <div>
-    <h3>当前最新的count:</h3>
-    <button>-1</button>
+    <!-- <h3>当前最新的count:{{$store.state.count}}</h3> -->
+    <h3>{{showNum}}</h3>
+    <button @click="btnHandler1">-1</button>
+    <button @click="btnHandler2">-N</button>
+    <button @click="btnHandler3">-1 Aysnc</button>
+    <button @click="btnHandler4">-N Aysnc</button>
   </div>
-</template> 
+</template>
 
 <script>
+import { mapState, mapMutations, mapActions, mapGetters } from 'vuex'
 export default {
-  data() {
+  data () {
     return {}
+  },
+  computed: {
+    ...mapState(['count']),
+    ...mapGetters(['showNum'])
+  },
+  methods: {
+    ...mapMutations(['sub', 'subN']),
+    ...mapActions(['subAsync', 'subNAsync']),
+    btnHandler1() {
+      this.sub()
+    },
+    btnHandler2() {
+      this.subN(3)
+    },
+    btnHandler3() {
+      this.subAsync()
+    },
+    btnHandler4() {
+      this.subNAsync(4)
+    }
   }
 }
-</script> 
+</script>
